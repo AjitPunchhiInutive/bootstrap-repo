4377c36607885eb5dbc54408a9e195a4994c1c24
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-02-24 22:35:48 +0530

    updated file

--- main.tf
+++ main.tf
@@ -29,6 +29,6 @@ locals {
 # }
 
   module "folders" {
-  source          = "git@github.com:AjitPunchhiInutive/simple-module.git//folders?ref=main"
+  source          = "git@github.com:AjitPunchhiInutive/bootstrap-repo.git//folders?ref=main"
   folders_objects = local.folders_objects
 }
