1af0b3df4b31a0bfe45cab8f32d7de2f62eebdab
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-02-25 00:21:42 +0530

    updated file

--- main.tf
+++ main.tf
@@ -12,23 +12,23 @@ locals {
     default_cost_center = local.default_cost_center
   }
 
-#   # WIF: list from config/wif/*.yaml
-#   wif_config_files = fileset("config/wif", "*.yaml")
-#   wif_objects = [for f in local.wif_config_files : yamldecode(templatefile("config/wif/${f}", local.template_vars))]
+  # WIF: list from config/wif/*.yaml
+  wif_config_files = fileset("config/wif", "*.yaml")
+  wif_objects = [for f in local.wif_config_files : yamldecode(templatefile("config/wif/${f}", local.template_vars))]
 
-  # Folders: single object from config/folders.yaml (module expects only organization_id, parent_folders, sub_folders)
-  folder_config_files = fileset("config/folders", "*.yaml")
-  folders_raw         = length(local.folder_config_files) > 0 ? yamldecode(templatefile("config/folders/folders.yaml", local.template_vars)) : null
-  #folders_objects     = local.folders_raw != null ? { organization_id = local.folders_raw.organization_id, parent_folders = local.folders_raw.parent_folders, sub_folders = local.folders_raw.sub_folders } : { organization_id = var.organization_id, parent_folders = {}, sub_folders = {} }
+#   # Folders: single object from config/folders.yaml (module expects only organization_id, parent_folders, sub_folders)
+#   folder_config_files = fileset("config/folders", "*.yaml")
+#   folders_raw         = length(local.folder_config_files) > 0 ? yamldecode(templatefile("config/folders/folders.yaml", local.template_vars)) : null
+#   #folders_objects     = local.folders_raw != null ? { organization_id = local.folders_raw.organization_id, parent_folders = local.folders_raw.parent_folders, sub_folders = local.folders_raw.sub_folders } : { organization_id = var.organization_id, parent_folders = {}, sub_folders = {} }
 
 }
 
-# module "wif_factory" {
-#   source                         = "git@github.com:southwire-company/gcp-shared-terraform-modules.git//wif-factory?ref=main"
-#   github_workload_identity_factory = local.wif_objects
+module "wif_factory" {
+  source                         = "git@github.com:southwire-company/gcp-shared-terraform-modules.git//wif-factory?ref=main"
+  github_workload_identity_factory = local.wif_objects
+}
+
+#   module "folders" {
+#   source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//folders?ref=main"
+#   folders_objects = local.folders_objects
 # }
-
-  module "folders" {
-  source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//folders?ref=main"
-  folders_objects = local.folders_objects
-}
