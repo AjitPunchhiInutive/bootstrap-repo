8951747ba8bc2154fe8fa40959c804298321a7ec
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-02-25 01:52:32 +0530

    updated file

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 locals {
-  default_cost_center = lookup(var.default_labels, "cost-center", "xxxx")
+#   default_cost_center = lookup(var.default_labels, "cost-center", "xxxx")
 
-  all_labels = merge(var.default_labels, {
-    lifecycle = var.environment
-    owner     = var.owner
-  })
+#   all_labels = merge(var.default_labels, {
+#     lifecycle = var.environment
+#     owner     = var.owner
+#   })
 
-  template_vars = {
-    environment_key     = var.environment_key
-    lifecycle           = var.environment
-    default_cost_center = local.default_cost_center
-  }
+#   template_vars = {
+#     environment_key     = var.environment_key
+#     lifecycle           = var.environment
+#     default_cost_center = local.default_cost_center
+#   }
 
   # WIF: list from config/wif/*.yaml
   wif_config_files = fileset("config/wif", "*.yaml")
-  wif_objects = [for f in local.wif_config_files : yamldecode(templatefile("config/wif/${f}", local.template_vars))]
+  wif_objects = [for f in local.wif_config_files : yamldecode(templatefile("config/wif/${f}"))]
 
 #   # Folders: single object from config/folders.yaml (module expects only organization_id, parent_folders, sub_folders)
 #   folder_config_files = fileset("config/folders", "*.yaml")
--- provider.tf
+++ provider.tf
@@ -17,12 +17,12 @@ provider "google" {
   project        = var.gcp_project
   region         = var.region
   zone           = var.zone
-  default_labels = local.all_labels
+  #default_labels = local.all_labels
 }
 
 provider "google-beta" {
   project        = var.gcp_project
   region         = var.region
   zone           = var.zone
-  default_labels = local.all_labels
+  #default_labels = local.all_labels
 }
