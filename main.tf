ccf9cadb18be4844f8a513d3ec10463723206884
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-03-03 00:50:36 +0530

    My Demo File

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ locals {
   folders_objects     = local.folders_raw != null ? { organization_id = local.folders_raw.organization_id, parent_folders = local.folders_raw.parent_folders, sub_folders = local.folders_raw.sub_folders } : { organization_id = var.organization_id, parent_folders = {}, sub_folders = {} }
 
   
-  project_config_files = fileset("config/project-factory", "*.yaml")
-  project_objects = [for f in local.project_config_files : yamldecode(file("config/project-factory/${f}"))]
+#   project_config_files = fileset("config/project-factory", "*.yaml")
+#   project_objects = [for f in local.project_config_files : yamldecode(file("config/project-factory/${f}"))]
 }
   # Projects: list from config/projects/*.yaml
 module "wif_factory" {
@@ -24,7 +24,7 @@ module "wif_factory" {
   folders_objects = local.folders_objects
 }
 
-module "project-factory" {
-  source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//project-factory?ref=main"
-  project_objects = local.project_objects
-}
+# module "project-factory" {
+#   source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//project-factory?ref=main"
+#   project_objects = local.project_objects
+# }
